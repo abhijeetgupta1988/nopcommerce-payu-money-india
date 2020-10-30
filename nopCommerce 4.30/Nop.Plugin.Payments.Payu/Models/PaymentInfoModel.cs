159f3a890e4879efa17ac0c9283aa8e217472adc
abhijeetgupta1988 <abhijeet.gupta1988@gmail.com>
2020-10-30 20:27:23 +0530

    Payu Plugin for Nop4.30 Added.

---
+++ nopCommerce 4.30/Nop.Plugin.Payments.Payu/Models/PaymentInfoModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc;
+using Nop.Web.Framework.Models;
+
+namespace Nop.Plugin.Payments.Payu.Models
+{
+    public class PaymentInfoModel : BaseNopModel
+    {
+        public PaymentInfoModel() { }
+    }
+}
