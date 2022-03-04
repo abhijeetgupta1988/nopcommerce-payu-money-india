07922c1169c120318baa1fe9e4fa1947dce5fc10
abhijeetgupta1988 <abhijeet.gupta1988@gmail.com>
2022-03-04 16:00:21 +0530

    Plugin added for Nop4.5
    Plugin added for Nop4.5

---
+++ nopCommerce 4.50/Nop.Plugin.Payments.Payu/PayuPaymentSettings.cs
@@ -0,0 +1,18 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Payments.Payu
+{
+    public class PayuPaymentSettings : ISettings
+    {
+        public string MerchantId { get; set; }
+        public string Key { get; set; }
+        public string MerchantParam { get; set; }
+        public string PayUri { get; set; }
+        public decimal AdditionalFee { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to "additional fee" is specified as percentage. true - percentage, false - fixed value.
+        /// </summary>
+        public bool AdditionalFeePercentage { get; set; }
+    }
+}
